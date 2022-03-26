84cf278ba0fbb7b3161c31a10e7848732b61deb5
Ruslanushka <sakharbekov@mail.ru>
2022-03-26 14:56:47 +0700

    lesson 11

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import News
 
 def index(request):
-    return HttpResponse("Hello")
+    news = News.objects.order_by('-create_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+
+    return render(request, 'news/base.html', context=context)
