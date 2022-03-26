0ff2f62b69ab03702fd114c6558e636ef3778368
Ruslanushka <sakharbekov@mail.ru>
2022-03-26 17:02:13 +0700

    lesson 13

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import News, Category
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'create_at', 'create_up', 'category', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content', 'id',)
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category', 'title')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title',)
+    search_fields = ('id', 'title',)
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+
+
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,13 +3,32 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name="Заголовок")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
-    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-    create_up = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    create_up = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото", blank=True)
     is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категория")
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-create_at', 'title']
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Название")
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
 
 
 
