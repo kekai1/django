50626a983b07e697c6ade54ba4de74489b53e451
Ruslanushka <sakharbekov@mail.ru>
2022-03-30 08:32:27 +0700

    lesson 20

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,23 +3,19 @@ from .models import *
 
 def index(request):
     news = News.objects.order_by('-create_at')
-    category = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей',
-        'category': category
+        'title': 'Список новостей'
     }
     return render(request, 'news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    category = Category.objects.all()
     this_category = Category.objects.get(pk=category_id)
     context = {
         'news': news,
         'title': 'Категории',
-        'category': category,
         'this_category': this_category
     }
     return render(request, 'news/category.html', context=context)
